e3c92fa9b0feede1845d22a744549033d8cacb1c
yaroslavsf <yaroslav1598753@gmail.com>
2023-02-22 16:47:02 +0100

    Add methods to manipulate user

--- src/main/java/com/example/webshop/domain/user/UserService.java
+++ src/main/java/com/example/webshop/domain/user/UserService.java
@@ -1,7 +1,12 @@
 package com.example.webshop.domain.user;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface UserService {
-    public List<User> findAll();
+    List<User> findAll();
+    User findById(UUID id);
+    User save(User user);
+    void deleteById(UUID id);
+    void updateUser(User user);
 }
--- src/main/java/com/example/webshop/domain/user/UserServiceImpl.java
+++ src/main/java/com/example/webshop/domain/user/UserServiceImpl.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -17,5 +20,33 @@ public class UserServiceImpl implements UserService{
     @Override
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(UUID id) {
+        Optional<User> userFromDb =  userRepository.findById(id);
+        if (!userFromDb.isPresent()) throw new NoSuchElementException();
+        User u = userFromDb.get();
+        return u;
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        Optional<User> userFromDb = userRepository.findById(user.getId());
+        if (!userFromDb.isPresent()) throw new NoSuchElementException();
+        // crush the variables of the object found
+        User u = userFromDb.get();
+        u.setName(user.getName());
+        u.setSurname(user.getSurname());
+        userRepository.save(u);
     }
 }
